04099e5df39c5d0d3ab50af05b854cb19dd19a09
Dmitry Spodarets <d.spodarets@flyelephant.net>
2015-11-01 01:29:25 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-R test.r >> result.dat
+R test.r >> result.dat 2>&1
 R --version >> result.dat
-sudo apt-get install  --yes --force-yes r-base-core >> result.dat 
+sudo apt-get install  --yes --force-yes r-base-core >> result.dat 2>&1
 R --version >> result.dat
-Rscript test.r >> result.dat
+Rscript test.r >> result.dat 2>&1
 
