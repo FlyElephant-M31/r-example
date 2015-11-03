e9487463752c94920ec296675d76604f59ae984e
Dmitry Spodarets <d.spodarets@flyelephant.net>
2015-11-03 12:31:13 -0800

    Update test.f

--- test.f
+++ test.f
@@ -1,9 +1,3 @@
-x <- 1:10
-y <- runif(5)
-f <- function(x){ x^2 }
-
-save(y, f, file="result.dat")
-rm(list=ls()) # remove all objects
-
-load("result.dat")
-ls()
+program hello
+print *,"Hello World!"
+end program hello
