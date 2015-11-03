92f99585c5281722babddbb30f8720531a539c17
Dmitry Spodarets <d.spodarets@flyelephant.net>
2015-11-03 12:34:23 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-R test.r >> result.dat 2>&1
-R --version >> result.dat
-sudo apt-get install  --yes --force-yes r-base-core >> result.dat 2>&1
-R --version >> result.dat
-Rscript test.r >> result.dat 2>&1
-
+gcc -v
+gfortran -v
+gfortran -ffree-form test.f
+./a.out
