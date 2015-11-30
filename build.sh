425c2a6242a3e6362d2533030e6841c9e5ea7ea4
Dmitry Spodarets <d.spodarets@flyelephant.net>
2015-11-30 15:44:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-uname -a
-cat result.dat >> result.dat 2>&1
-octave -v >> result.dat 2>&1
-octave test.m >> result.dat 2>&1
+uname -a >> result.dat 2>&1
 cd 1
 octave Go_MakeTrain.m >> result.dat 2>&1
