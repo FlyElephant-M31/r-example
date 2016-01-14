0b799bc6b6c0b9e0229cceeaea413840672f86aa
Dmitry Spodarets <d.spodarets@flyelephant.net>
2016-01-14 13:33:19 +0200

    Update test.r

--- test.r
+++ test.r
@@ -1,9 +1,8 @@
-x <- 1:10
-y <- runif(5)
-f <- function(x){ x^2 }
-
-save(y, f, file="result.dat")
-rm(list=ls()) # remove all objects
-
-load("result.dat")
-ls()
+install.packages("igraph")
+install.packages("dplyr")
+library(igraph)
+library(dplyr)
+g <- random.graph.game(n = 1000000, p.or.m = 1/4, directed = TRUE)
+pr <- page.rank(g)$vector
+df <- data.frame(Object = 1:10, PageRank = pr)
+arrange(df, cyl(PageRank))
